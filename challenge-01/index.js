8c4369156d4444b509e49f61372c739a3cb036e2
Yasminn Vaz <yasminnvaz@gmail.com>
2020-12-02 12:52:25 -0300

    refactor challenge-01

--- challenge-01/index.js
+++ challenge-01/index.js
@@ -1,37 +1,48 @@
-const fs = require('fs');
-
-const content = fs.readFileSync('./data.txt', 'utf-8').split('\n').map(Number);
+const data = require('./data')
 
 function findTwoNumbers(arr) {
+    const goal = 2020
     let nums
 
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             let sum = arr[i] + arr[j]
-            if (sum === 2020) {
+            if (sum === goal) {
                 nums = [arr[i], arr[j]]
-                console.log(nums)
             }
         }
     }
+
+    return nums
 }
 
 function findThreeNumbers(arr) {
+    const goal = 2020
     let nums
 
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             for (let h = j + 1; h < arr.length; h++) {
                 let sum = arr[i] + arr[j] + arr[h]
-                if (sum === 2020) {
+                if (sum === goal) {
                     nums = [arr[i], arr[j], arr[h]]
-                    console.log(nums)
-                    return
+                    break;
                 }
             }
         }
     }
+
+    return nums
 }
 
-findTwoNumbers(content)
-findThreeNumbers(content)
+function findSecretNumber(arr) {
+    const secretNumber = arr.reduce((acc, curr) => acc * curr);
+    return secretNumber
+}
+
+const nums1 = findTwoNumbers(data)
+const nums2 = findThreeNumbers(data)
+
+console.log(findSecretNumber(nums1));
+console.log(findSecretNumber(nums2));
+
