760238969bec3fe161e12e3ce340c0e412c4c59a
Yasminn Vaz <yasminnvaz@gmail.com>
2020-12-06 23:02:29 -0300

    [challenge-04] adds part two

--- challenge-04/index.js
+++ challenge-04/index.js
@@ -5,25 +5,61 @@ const requiredFields = fs.readFileSync('./requiredFields.txt', 'UTF-8')
     .map(item => item.replace(/\s.+/, ''))
     .filter(x => x)
 
-const example = fs.readFileSync('./example-04.txt', 'UTF-8').split(/\n\n/)
 const data = fs.readFileSync('./data-04.txt', 'UTF-8').split(/\n\n/)
 
 function parserPassportsFields(passports) {
-    return passports.map(passport => new Map(passport
+    return passports.map(passport => (passport
         .split(/\s+/)
         .filter(x => x)
-        .map(field => field.split(/:/))))
+        .map(field => field.split(/:/))
+        .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {})))
 }
 
-function countValidPassports(passports, requiredFields) {
-    const { length } = passports
-        .filter(passport => requiredFields.every(field => passport.has(field)))
+function validatePassportsByFields(passports, requiredFields) {
+    return passports
+        .filter(passport => requiredFields
+            .filter(requiredField => requiredField !== 'cid')
+            .every(field => passport.hasOwnProperty(field)))
+}
 
-    return length
+function validatePassportsByRules({ byr, iyr, eyr, hgt, hcl, ecl, pid }) {
+    const byrCheck = checkRange(byr, 1920, 2002);
+    const iyrCheck = checkRange(iyr, 2010, 2020);
+    const eyrCheck = checkRange(eyr, 2020, 2030);
+    const hgtCheck = checkHeight(hgt)
+    const hclCheck = /^#[0-9a-f]{6}$/.test(hcl)
+    const eclCheck = /^(amb|blu|brn|gry|grn|hzl|oth)$/.test(ecl)
+    const pidCheck = /^\d{9}$/g.test(pid)
+
+    const result = { byrCheck, iyrCheck, eyrCheck, hgtCheck, hclCheck, eclCheck, pidCheck }
+
+    return Object.values(result).every(item => item)
+}
+
+function checkRange(number, min, max) {
+    return (number >= min && number <= max)
+}
+
+function checkHeight(height) {
+    const hgtRule = /^(\d+)(cm|in)$/
+
+    if (hgtRule.test(height)) {
+        const [_, value, unit] = height.match(hgtRule)
+        if (unit === 'cm') {
+            return value >= 150 && value <= 193
+        } else {
+            return value >= 59 && value <= 76
+        }
+    } else {
+        return false
+    }
 }
 
 const passports = parserPassportsFields(data)
-const validPassports = countValidPassports(passports, requiredFields)
 
-console.log(`Total: ${validPassports}`)
+const validByFields = validatePassportsByFields(passports, requiredFields)
+console.log(`Valid by fields: ${validByFields.length}`)
+
+const validByRules = passports.filter(passport => validatePassportsByRules(passport))
+console.log(`Valid by rules: ${validByRules.length}`)
 
